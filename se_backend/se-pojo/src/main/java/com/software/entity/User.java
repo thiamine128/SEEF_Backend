0dd9af366b1ff60c9d528a905696eef38152b355
陈恒康 <22371179>
2024-04-16 18:58:11 +0800

    user类

---
+++ se_backend/se-pojo/src/main/java/com/software/entity/User.java
@@ -0,0 +1,56 @@
+package com.software.entity;
+
+import java.util.Date;
+
+/**
+ * @author
+ * @Description：
+ * @date
+ */
+public class User {
+    private Long id;
+
+    /**
+     * 用户账号
+     */
+    private String userAccount;
+
+    /**
+     * 用户密码
+     */
+    private String userPassword;
+    /**
+     * 用户昵称
+     */
+    private String userName;
+
+    /**
+     * 用户头像
+     */
+    private String userAvatar;
+
+    /**
+     * 用户简介
+     */
+    private String userProfile;
+
+    /**
+     * 用户角色：user/admin/ban
+     */
+    private String userRole;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    private Integer isDelete;
+}
