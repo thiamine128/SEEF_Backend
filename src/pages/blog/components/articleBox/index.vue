4517d35d70c70a72cb494fd7ebaaf34280d7ae7c
阮阳栋 <14192765+RyotoTannhauser@user.noreply.gitee.com>
2024-04-23 12:10:18 +0800

    more components for blog

---
+++ src/pages/blog/components/articleBox/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="box-container">
+        <img class="img-adjust" src="@/assets/blog/testArticleImg.png" alt="404 not found">
+        <div class="msg">
+            <div :class="{ titleFont: true }">{{title}}</div>
+            <div :class="{ abstractFont: true }">{{abstract}}</div>
+            <div class="otherMsg">
+                <div :class="{ otherMsgFont: true }">{{"作者："+author}}</div>
+                <div :class="{ otherMsgFont: true }">{{" | 发布时间："+postTime}}</div>
+                <div :class="{ otherMsgFont: true }">{{" | 点赞："+likes}}</div>
+            </div>
+        </div>
+    </div>
+    <hr class="setHr">
+</template>
+
+<script>
+export default {
+    name: "articleBox",
+    props:['title', 'abstract', 'author', 'postTime', 'likes']
+}
+</script>
+
+<style scoped>
+.box-container{
+    height: 100px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    margin: 4px;
+}
+.img-adjust{
+    width: 25%;
+    height: 100%;
+}
+.msg{
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+}
+.otherMsg{
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    margin-top: auto;
+}
+.titleFont {
+    font-family: '微软雅黑', 'Microsoft YaHei', sans-serif;
+    font-weight: bold;
+    font-size: 25px;
+    margin-left: 5px;
+    text-align: left;
+}
+.abstractFont {
+    font-family: '微软雅黑', 'Microsoft YaHei', sans-serif;
+    font-size: 12px;
+    margin-left: 5px;
+    color: #8e8e8e;
+    text-align: left;
+}
+.otherMsgFont {
+    font-family: '微软雅黑', 'Microsoft YaHei', sans-serif;
+    font-size: 14px;
+    margin-left: 5px;
+    color: #8e8e8e;
+    text-align: left;
+}
+.setHr{
+    border-top-color: rgba(44, 44, 44, 0.1);
+}
+</style>
---
+++ src/pages/blog/components/sectionBox/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="box-container">
+        <div class="titleSet">
+            <img src="/favicon.ico" alt="404 not found">
+            <div :class="{ titleFont: true }">{{title}}</div>
+            <div :class="{ abstractFont: true }">{{abstract}}</div>
+        </div>
+        <div :class="{ moreSet: true }"> > </div>
+    </div>
+    <hr class="setHr">
+</template>
+
+<script>
+export default {
+    name: "sectionBox",
+    props: ['title', 'abstract']
+}
+</script>
+
+<style scoped>
+.setHr{
+    border-top-color: rgba(44, 44, 44, 0.1);
+}
+.box-container{
+    display: flex;
+    justify-content: space-between;
+    margin: 5px;
+}
+.titleFont {
+    font-family: '微软雅黑', 'Microsoft YaHei', sans-serif;
+    font-weight: bold;
+    font-size: 25px;
+    margin-left: 10px;
+    text-align: left;
+}
+.moreSet{
+    font-family: '微软雅黑', 'Microsoft YaHei', sans-serif;
+    font-size: 25px;
+    margin-right: 5px;
+    text-align: right;
+    color: #8e8e8e;
+}
+.abstractFont {
+    font-family: '微软雅黑', 'Microsoft YaHei', sans-serif;
+    font-size: 12px;
+    margin-left: 5px;
+    color: #8e8e8e;
+    text-align: left;
+}
+.titleSet{
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    align-items: center;
+}
+</style>
