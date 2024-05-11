767b9505cd998e005d36254806e8d0b5ba1e3733
闫佳豪 <21377242>
2024-05-11 22:45:36 +0800

    add courses syllabus

--- src/pages/education/subPages/courses/internetMarketing/index.vue
+++ src/pages/education/subPages/courses/internetMarketing/index.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="bg-container"/>
     <navigation-bar/>
-    <course-info :course="course"/>
+    <course-info :course="course" />
+
+
 
 </template>
 
@@ -22,10 +24,66 @@ export default {
                 location: "主南303",
                 credits: 2,
                 assessment: "平时作业+课堂展示",
-                image: "../../../../assets/education/course/互联网营销.png"
-                // image: "@/assets/education/course/数据库.jpg"
+                image: "../../../../assets/education/course/互联网营销.png",
+                detailedContent:
+                    '# 互联网营销课程大纲\n' +
+                    '\n' +
+                    '## 课程简介\n' +
+                    '本课程旨在介绍互联网营销的基本概念、策略和工具，帮助学生了解如何利用互联网平台进行营销推广，提升品牌知名度和销售业绩。\n' +
+                    '\n' +
+                    '## 课程目标\n' +
+                    '- 理解互联网营销的基本原理和发展趋势\n' +
+                    '- 掌握各种互联网营销工具和技术的运用\n' +
+                    '- 能够制定和实施有效的互联网营销策略\n' +
+                    '- 了解互联网营销在不同行业和平台上的应用案例\n' +
+                    '\n' +
+                    '## 主要内容\n' +
+                    '1. 互联网营销概述\n' +
+                    '   - 互联网营销的定义和发展历程\n' +
+                    '   - 互联网营销与传统营销的区别和联系\n' +
+                    '   - 互联网营销的基本原理和模式\n' +
+                    '\n' +
+                    '2. 互联网营销策略\n' +
+                    '   - 品牌定位与传播\n' +
+                    '   - 内容营销与社交媒体营销\n' +
+                    '   - 搜索引擎优化（SEO）与搜索引擎营销（SEM）\n' +
+                    '   - 电子邮件营销与移动营销\n' +
+                    '\n' +
+                    '3. 互联网营销工具与技术\n' +
+                    '   - 网站建设与用户体验优化\n' +
+                    '   - 数据分析与用户行为跟踪\n' +
+                    '   - 社交媒体平台的应用与管理\n' +
+                    '   - 互联网广告投放与效果评估\n' +
+                    '\n' +
+                    '4. 互联网营销案例分析\n' +
+                    '   - 不同行业的互联网营销实践\n' +
+                    '   - 成功的互联网营销案例解析\n' +
+                    '   - 互联网营销在国际市场的应用\n' +
+                    '\n' +
+                    '## 教学方法\n' +
+                    '- 理论讲授\n' +
+                    '- 案例分析\n' +
+                    '- 小组讨论\n' +
+                    '- 实地考察\n' +
+                    '- 课程项目\n' +
+                    '\n' +
+                    '## 考核方式\n' +
+                    '- 平时表现\n' +
+                    '- 课堂互动\n' +
+                    '- 课程项目报告\n' +
+                    '- 期末考试\n' +
+                    '\n' +
+                    '以上是一个简单的互联网营销课程大纲，你觉得如何？'
             }
         }
+    },
+    methods: {
+        // showDetail() {
+        //     this.show = true;
+        // },
+        // closeDetail() {
+        //     this.show = false;
+        // }
     }
 }
 </script>
@@ -62,5 +120,12 @@ export default {
     justify-content: center;
     margin-top: 20px;
     width: 1200px;
+}
+
+.close-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
 }
 </style>
