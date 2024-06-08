3d53fd5e74a95c60762ef9952b949bc225576f12
陈恒康 <22371179>
2024-06-09 00:00:20 +0800

    删除班级

--- se_backend/se-pojo/src/main/java/com/software/dto/BlogPreviewPageQueryDTO.java
+++ se_backend/se-pojo/src/main/java/com/software/dto/BlogPreviewPageQueryDTO.java
@@ -2,6 +2,7 @@ package com.software.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 import java.io.Serializable;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BlogPreviewPageQueryDTO implements Serializable {
     private int page;
     private int pageSize;
